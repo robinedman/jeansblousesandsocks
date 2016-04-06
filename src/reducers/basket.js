a9a5ce9eeadc5731bc8d7b634d3ffe698eed647a
Robin Edman <robin.edman@gmail.com>
2016-04-06 13:16:45 +0100

    Changed basket data structure

--- src/reducers/basket.js
+++ src/reducers/basket.js
@@ -1,17 +1,30 @@
+/*
+Example basket:
+{
+  items: {
+    "B01": { "id":"B01","product":"Blouse","price":24.95,"amount":2 }
+  },
+  total: 49.9
+}
+*/
 const initialState = {
-  items: [],
+  items: {},
   total: 0
 }
 
-const calculateTotal = (items) => items.reduce((sum, item) => sum + item.price, 0)
+const calculateTotal = (items) => {
+  return Object.keys(items).reduce((sum, itemKey) => sum + items[itemKey].price * items[itemKey].amount, 0)
+}
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
-      const newBasketItemsState = [
-        ...state.items,
-        action.item
-      ]
+      let newBasketItemsState = { ...state.items }
+      newBasketItemsState[action.item.id] = {
+        ...action.item,
+        amount: (state.items[action.item.id] && state.items[action.item.id].amount || 0) + 1
+
+      }
       return {
         ...state,
         items: newBasketItemsState,
