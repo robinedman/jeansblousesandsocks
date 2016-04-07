3d602d783116e8f012aa772fe370c2476eb8f1ec
Robin Edman <robin.edman@gmail.com>
2016-04-07 12:05:45 +0100

    Extracted basket rules

---
+++ src/basketRules.js
@@ -0,0 +1,35 @@
+/*
+  Ways to calculate item price.
+*/
+const twoForOne = item => (
+  item.amount % 2 === 0
+    ? item.amount / 2 * item.price
+    : ((item.amount - 1) / 2 * item.price) + item.price
+)
+const secondHalfPrice = item => (
+  item.amount % 2 === 0
+    ? ((item.amount / 2) * item.price) + ((item.amount / 2) * (item.price / 2))
+    : (((item.amount - 1) / 2) * item.price) + (((item.amount - 1) / 2) * (item.price / 2)) + item.price
+)
+const regular = item => item.price * item.amount
+
+
+export const getTotalExcludingShipping = items => Object.keys(items).reduce((sum, itemKey) => {
+  const item = items[itemKey]
+  const itemCost = item.offer === 'secondHalfPrice' ? secondHalfPrice(item) : regular(item)
+
+  return sum + itemCost
+}, 0)
+
+// Note: Spec doesn't specify what happens for orders of exactly £90.
+// Making it free.
+export const getShipping = cost => {
+  if (cost < 50) return 4.95
+  if (cost < 90) return 2.95
+  return 0
+}
+
+export const getTotal = items => {
+  const totalExcludingShipping = getTotalExcludingShipping(items)
+  const shipping = getShipping(totalExcludingShipping)
+}
--- src/reducers/basket.js
+++ src/reducers/basket.js
@@ -1,3 +1,8 @@
+import {
+  getTotalExcludingShipping,
+  getShipping,
+  getTotal } from '../basketRules'
+
 /*
 Example basket:
 {
@@ -11,39 +16,6 @@ const initialState = {
   items: {},
   total: 0
 }
-
-const twoForOne = item => (
-  item.amount % 2 === 0
-    ? item.amount / 2 * item.price
-    : ((item.amount - 1) / 2 * item.price) + item.price
-)
-const secondHalfPrice = item => (
-  item.amount % 2 === 0
-    ? ((item.amount / 2) * item.price) + ((item.amount / 2) * (item.price / 2))
-    : (((item.amount - 1) / 2) * item.price) + (((item.amount - 1) / 2) * (item.price / 2)) + item.price
-)
-const regular = item => item.price * item.amount
-
-const getTotalExcludingShipping = items => Object.keys(items).reduce((sum, itemKey) => {
-  const item = items[itemKey]
-  const itemCost = item.offer === 'secondHalfPrice' ? secondHalfPrice(item) : regular(item)
-
-  return sum + itemCost
-}, 0)
-
-// Note: Spec doesn't specify what happens for orders of exactly £90.
-// Making it free.
-const getShipping = cost => {
-  if (cost < 50) return 4.95
-  if (cost < 90) return 2.95
-  return 0
-}
-
-const getTotal = items => {
-  const totalExcludingShipping = getTotalExcludingShipping(items)
-  const shipping = getShipping(totalExcludingShipping)
-}
-
 
 export default (state = initialState, action) => {
   let newItemsState
